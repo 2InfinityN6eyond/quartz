ba1bad3f0c2f340f6599538762214b13ba21a7a4
Hyunje Park <hjp1nskku@g.skku.edu>
2023-09-06 01:54:54 +0900

    Quartz sync: Sep 6, 2023, 1:54 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Hyunje Park's Mind Map",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
